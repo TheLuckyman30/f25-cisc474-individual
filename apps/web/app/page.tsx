da80ed173f24342bbb1f2b73478f048c517fe2eb
TheLuckyman30 <bnauta0@gmail.com>
2025-09-11 21:28:46 -0400

    Added initial styling to home page

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -2,9 +2,13 @@ import Link from 'next/link';
 
 export default function Home() {
   return (
-    <div>
-      <div>Home Page</div>
-      <Link href="/home/dashboard">Login</Link>
+    <div className="h-full w-full flex flex-wrap flex-col justify-center items-center">
+      <div className="font-bold text-2xl md:text-7xl">
+        Welcome to [NAME] LMS
+      </div>
+      <Link href="/home/dashboard" className="absolute top-0 right-0 m-5">
+        Login | Signup
+      </Link>
     </div>
   );
 }
