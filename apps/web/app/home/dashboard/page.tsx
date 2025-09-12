852c871757a865fabc53f6967f1da82f9544b1aa
TheLuckyman30 <bnauta0@gmail.com>
2025-09-12 01:58:30 -0400

    Added dropdown to control which type of info is displayed on the dashboard

--- apps/web/app/home/dashboard/page.tsx
+++ apps/web/app/home/dashboard/page.tsx
@@ -1,18 +1,41 @@
+'use client';
+
 import CourseCard from './components/course-card';
 import { courses, assignments } from '../../temp-data/temp-data.json';
+import {
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select,
+  SelectChangeEvent,
+} from '@mui/material';
+import { useState } from 'react';
 
 function Dashboard() {
+  const [displayedInfo, setDisplayedInfo] = useState<string>('Courses');
+
+  function handleDisplayInfoChange(event: SelectChangeEvent) {
+    setDisplayedInfo(event.target.value);
+  }
+
   return (
-    <div className="flex flex-col justify-center items-center h-lvh w-lvw">
-      <div className="w-fit">
-        <div className="flex justify-between mb-25">
-          <div>Dropdown</div>
+    <div className="flex justify-center items-center h-lvh w-lvw">
+      <div className="grid grid-rows-1 gap-25 w-fit">
+        <div className="flex justify-between">
+          <FormControl>
+            <InputLabel>Select Info</InputLabel>
+            <Select value={displayedInfo} onChange={handleDisplayInfoChange}>
+              <MenuItem value={'Courses'}>Courses</MenuItem>
+              <MenuItem value={'Assignments'}>Assignments</MenuItem>
+            </Select>
+          </FormControl>
           <div>Dropdown</div>
         </div>
         <div className="grid grid-cols-5 gap-15">
-          {courses.map((course, index) => (
-            <CourseCard course={course} key={index} />
-          ))}
+          {displayedInfo === 'Courses' &&
+            courses.map((course, index) => (
+              <CourseCard course={course} key={index} />
+            ))}
         </div>
       </div>
     </div>
