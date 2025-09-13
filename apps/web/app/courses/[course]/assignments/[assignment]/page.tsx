c2f7664c581f806b39ffbd790ff98b25a0041f74
TheLuckyman30 <bnauta0@gmail.com>
2025-09-12 22:36:07 -0400

    Added content to assignment page

--- apps/web/app/courses/[course]/assignments/[assignment]/page.tsx
+++ apps/web/app/courses/[course]/assignments/[assignment]/page.tsx
@@ -9,7 +9,17 @@ async function Assignment({
   const selectedAssignment = assignments.find((a) => a.id === assignment);
 
   if (selectedAssignment) {
-    return <div>{selectedAssignment.name}</div>;
+    return (
+      <div className="mt-30 ml-30">
+        <div className="font-bold text-3xl">{selectedAssignment.name}</div>
+        <div className="text-2xl flex gap-5">
+          <div>{`Due Date: ${selectedAssignment.dueDate}`}</div>
+          <div>Submission Type: PDF</div>
+        </div>
+        <div className="text-2xl">Content</div>
+        <div>{selectedAssignment.description}</div>
+      </div>
+    );
   }
 }
 
