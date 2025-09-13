bceae85266f5eb377d1e840b2f67327e0ba380dd
TheLuckyman30 <bnauta0@gmail.com>
2025-09-12 23:43:04 -0400

    Re-added website title

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -4,7 +4,9 @@ export default function Home() {
   return (
     <div className="h-full w-full flex flex-wrap flex-col justify-center items-center">
       <div className="flex flex-col gap-10 items-center">
-        <div className="font-bold text-2xl md:text-7xl">Welcome to my LMS</div>
+        <div className="font-bold text-2xl md:text-7xl">
+          Welcome to my custom LMS
+        </div>
         <div className="text-2xl">
           Press the Login/Signup button in the top right to get started
         </div>
