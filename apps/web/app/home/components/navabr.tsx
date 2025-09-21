3a22bb477a218d9e4eb9553bc66703cee8a92d0c
TheLuckyman30 <bnauta0@gmail.com>
2025-09-20 21:31:23 -0400

    Initial redesign of the navbar

--- apps/web/app/home/components/navabr.tsx
+++ apps/web/app/home/components/navabr.tsx
@@ -1,23 +1,27 @@
 'use client';
 import Link from 'next/link';
-import { useState } from 'react';
+import { usePathname } from 'next/navigation';
 
 const NAV_LINKS: string[] = ['Dashboard', 'Calendar', 'Messages', 'Help'];
 
 function NavBar() {
-  const [currentPage, setCurrentPage] = useState<number>(0);
-
-  function setNewPage(index: number) {
-    setCurrentPage(index);
-  }
-
+  const pathName = usePathname();
   return (
     <div className="flex justify-between items-center fixed w-full h-fit p-10 bg-white shadow-md">
       <div>Site Page</div>
       <div className="flex items-center gap-15 rounded-md p-3 bg-gray-200/30">
-        {NAV_LINKS.map((link, index) => (
-          <div key={index}>{link}</div>
-        ))}
+        {NAV_LINKS.map((link, index) => {
+          const newLink = `/home/${link.toLowerCase()}`;
+          return (
+            <Link
+              href={newLink}
+              className={`hover:text-blue-400 duration-75 cursor-pointer ${newLink === pathName ? 'text-blue-400' : ''}`}
+              key={index}
+            >
+              {link}
+            </Link>
+          );
+        })}
       </div>
       <div>Some button probably</div>
     </div>
