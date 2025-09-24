11116014fd51d943617b1efc68032fe48b702735
TheLuckyman30 <bnauta0@gmail.com>
2025-09-23 23:04:04 -0400

    Added assignment card component to dashboard and assignments are now viewable on the dashboard

---
+++ apps/web/app/home/dashboard/components/assignment-card.tsx
@@ -0,0 +1,42 @@
+import Link from 'next/link';
+import { Assignment, Course } from '../../../temp-data/temp-interfaces';
+import { MoveRight } from 'lucide-react';
+import { courses } from '../../../temp-data/temp-data.json';
+
+interface AssignmentCardProps {
+  assignment: Assignment;
+}
+
+function AssingmentCard({ assignment }: AssignmentCardProps) {
+  const course: Course | undefined = courses.find(
+    (c) => c.id === assignment.courseId,
+  );
+
+  if (course) {
+    return (
+      <div className="flex flex-col justify-between rounded-md p-2 shadow-md bg-white border-b-blue-400 border-b-8 min-h-75 w-full md:w-75 ">
+        <div className="flex flex-col gap-2">
+          <div className="text-sm text-gray-500">{course.name}</div>
+          <div className="flex gap-2 justify-between ">
+            <div className="font-bold text-xl">{assignment.name}</div>
+            <Link
+              href={`/courses/${course.id}/assignments/${assignment.id}`}
+              className="flex h-fit w-fit bg-blue-400 rounded-md p-1 hover:scale-110 duration-100"
+            >
+              <MoveRight color="white" />
+            </Link>
+          </div>
+          <div>{assignment.description}</div>
+        </div>
+        <div className="flex w-full justify-center">
+          <div className="flex bg-blue-400 hover:scale-105 duration-100 rounded-md text-white gap-5 font-bold p-1 cursor-pointer">
+            <Link href={`/courses/${course.id}/course-home`}>Go to Course</Link>
+            <MoveRight color="white" />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default AssingmentCard;
--- apps/web/app/home/dashboard/page.tsx
+++ apps/web/app/home/dashboard/page.tsx
@@ -2,7 +2,9 @@
 import CourseCard from './components/course-card';
 import Dropdown from './components/dropdown';
 import { courses } from '../../temp-data/temp-data.json';
+import { assignments } from '../../temp-data/temp-data.json';
 import { useState } from 'react';
+import AssignmentCard from './components/assignment-card';
 
 const POSSIBLE_ITEMS = ['Courses', 'Assignments'];
 
@@ -10,18 +12,23 @@ function Dashboard() {
   const [selectedInfo, setSelectedInfo] = useState<string>('Courses');
 
   return (
-    <div className="flex justify-center items-center min-h-lvh pt-30">
-      <div className="flex flex-col gap-10 w-fit max-w-[90%]">
+    <div className="flex justify-center w-lvw min-h-lvh pt-[28vh]">
+      <div className="flex flex-col gap-10 w-[90%] 2xl:max-w-[60%]">
         <div className="font-bold text-4xl md:text-7xl">{selectedInfo}</div>
         <Dropdown
           possibleItems={POSSIBLE_ITEMS}
           selectedItem={selectedInfo}
           setSelectedItem={setSelectedInfo}
         />
-        <div className="flex flex-wrap gap-2">
-          {courses.map((course, index) => (
-            <CourseCard course={course} key={index} />
-          ))}
+        <div className="flex flex-wrap gap-2 ">
+          {selectedInfo === 'Courses' &&
+            courses.map((course, index) => (
+              <CourseCard course={course} key={index} />
+            ))}
+          {selectedInfo === 'Assignments' &&
+            assignments.map((assingment, index) => (
+              <AssignmentCard key={index} assignment={assingment} />
+            ))}
         </div>
       </div>
     </div>
