912d471d4c129d74635c40ff4499317554465ceb
TheLuckyman30 <bnauta0@gmail.com>
2025-10-04 18:45:29 -0400

    Assignments are now loaded for each course in the course routes

--- apps/web/app/courses/[course]/assignments/[assignment]/page.tsx
+++ apps/web/app/courses/[course]/assignments/[assignment]/page.tsx
@@ -1,26 +1,24 @@
-import { assignments } from '../../../../temp-data/temp-data.json';
+// async function Assignment({
+//   params,
+// }: {
+//   params: Promise<{ assignment: string }>;
+// }) {
+//   const { assignment } = await params;
+//   const selectedAssignment = assignments.find((a) => a.id === assignment);
 
-async function Assignment({
-  params,
-}: {
-  params: Promise<{ assignment: string }>;
-}) {
-  const { assignment } = await params;
-  const selectedAssignment = assignments.find((a) => a.id === assignment);
+//   if (selectedAssignment) {
+//     return (
+//       <div className="mt-30 ml-30">
+//         <div className="font-bold text-3xl">{selectedAssignment.name}</div>
+//         <div className="text-2xl flex gap-5">
+//           <div>{`Due Date: ${selectedAssignment.dueDate}`}</div>
+//           <div>Submission Type: PDF</div>
+//         </div>
+//         <div className="text-2xl">Content</div>
+//         <div>{selectedAssignment.description}</div>
+//       </div>
+//     );
+//   }
+// }
 
-  if (selectedAssignment) {
-    return (
-      <div className="mt-30 ml-30">
-        <div className="font-bold text-3xl">{selectedAssignment.name}</div>
-        <div className="text-2xl flex gap-5">
-          <div>{`Due Date: ${selectedAssignment.dueDate}`}</div>
-          <div>Submission Type: PDF</div>
-        </div>
-        <div className="text-2xl">Content</div>
-        <div>{selectedAssignment.description}</div>
-      </div>
-    );
-  }
-}
-
-export default Assignment;
+// export default Assignment;
