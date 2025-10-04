124ab253c1180c64baea11440b125c8cfda4aa7c
TheLuckyman30 <bnauta0@gmail.com>
2025-10-04 18:52:38 -0400

    Assignment page will now pull data from the database

--- apps/web/app/courses/[course]/assignments/[assignment]/page.tsx
+++ apps/web/app/courses/[course]/assignments/[assignment]/page.tsx
@@ -1,24 +1,20 @@
-// async function Assignment({
-//   params,
-// }: {
-//   params: Promise<{ assignment: string }>;
-// }) {
-//   const { assignment } = await params;
-//   const selectedAssignment = assignments.find((a) => a.id === assignment);
+import Assignment from './components/assignment';
 
-//   if (selectedAssignment) {
-//     return (
-//       <div className="mt-30 ml-30">
-//         <div className="font-bold text-3xl">{selectedAssignment.name}</div>
-//         <div className="text-2xl flex gap-5">
-//           <div>{`Due Date: ${selectedAssignment.dueDate}`}</div>
-//           <div>Submission Type: PDF</div>
-//         </div>
-//         <div className="text-2xl">Content</div>
-//         <div>{selectedAssignment.description}</div>
-//       </div>
-//     );
-//   }
-// }
+async function getAssignmentById(assignmentId: string) {
+  return fetch(process.env.BACKEND_URL + `/assignments/${assignmentId}`).then(
+    (response) => response.json(),
+  );
+}
 
-// export default Assignment;
+async function AssignmentPage({
+  params,
+}: {
+  params: Promise<{ assignment: string }>;
+}) {
+  const assignmentId = (await params).assignment;
+  const assignment = getAssignmentById(assignmentId);
+
+  return <Assignment assignment={assignment} />;
+}
+
+export default AssignmentPage;
